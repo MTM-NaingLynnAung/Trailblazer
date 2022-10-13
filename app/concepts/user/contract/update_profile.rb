63a775c4711de8a97092a6cdae304c86bbeaf429
NaingLynnAung <scm.nainglynnaung@gmail.com>
2022-10-13 16:48:18 +0630

    fix image error

--- app/concepts/user/contract/update_profile.rb
+++ app/concepts/user/contract/update_profile.rb
@@ -6,7 +6,8 @@ module User::Contract
     property :user_type
     property :address
     property :dob
-
+    property :image
+    
     validates :name, presence: true, length: { maximum: 100 }
     validates :email, presence: true, length: { maximum: 100 },
                       format: { with: /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i },
