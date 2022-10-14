74c6cdd01cc8af43252eff8025a757032128f32b
NaingLynnAung <scm.nainglynnaung@gmail.com>
2022-10-14 10:41:52 +0630

    adding error page

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+
   root 'login#login'
   post '/login', to: 'login#action_login'
   delete '/logout', to: 'login#logout'
@@ -25,5 +26,9 @@ Rails.application.routes.draw do
       get :filter
     end
   end
+
+  match "/404", to: "errors#not_found", via: :all
+  match "/500", to: "errors#internal_server_error", via: :all
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
