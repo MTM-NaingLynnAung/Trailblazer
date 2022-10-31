ef6796e0b49a7202dde438a8902ac4a04dc8cb8d
NaingLynnAung <scm.nainglynnaung@gmail.com>
2022-10-31 13:03:18 +0630

    add fade out in alert

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -14,3 +14,9 @@ require("channels")
 //
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
+
+$(document).on('turbolinks:load', function() {
+  setTimeout(function() {
+    $('.alert-dismissible').fadeOut();
+  }, 5000);
+})
